870b2e9af95486aeba3eb50dbaf1754a7bdabf14
joqd <gorgdis@gmail.com>
2025-08-31 01:45:48 +0330

    format code

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+	<h1>Index page</h1>
+	<div>This is root page</div>
+
+	<div class="p-5">
+		<UButton color="neutral" class="cursor-pointer">Button</UButton>
+	</div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+	modules: ["@nuxt/ui"],
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	css: ["~/assets/css/main.css"],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
