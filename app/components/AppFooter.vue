471f92a5dfa11e6268c4ea29bceb21c865ae2656
joqd <gorgdis@gmail.com>
2025-08-31 22:22:29 +0330

    Header & Footer components implemented

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="h-[20px]"></div>
+
+	<footer class="w-full h-[55px]">
+		<div class="flex h-[55px] justify-between px-[20px] max-w-[480px] mx-auto items-center">
+			<div class="text-sm opacity-70">© 2025 Joqd - Make with love</div>
+			<div class="opacity-70">
+				<ColorModeButton />
+			</div>
+		</div>
+	</footer>
+
+	<div class="h-[20px]"></div>
+</template>
---
+++ app/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="h-[30px]"></div>
+	<header class="w-full h-[60px] px-[20px] mb-[40px]">
+		<div
+			class="max-w-[440px] mx-auto grid grid-cols-2 sm:grid-cols-[max-content_minmax(0,1fr)_max-content] items-center"
+		>
+			<div class="flex items-center justify-start">
+				<NuxtLink to="/">
+					<UAvatar
+						src="/images/logo.jpg"
+						class="w-[60px] h-[60px] grayscale"
+					/>
+				</NuxtLink>
+			</div>
+
+			<div
+				class="sm:flex hidden justify-around px-11 font-semibold text-sm items-center"
+			>
+				<NuxtLink to="/" class="opacity-50 hover:opacity-100"
+					>Home</NuxtLink
+				>
+				<NuxtLink to="/now" class="opacity-50 hover:opacity-100"
+					>Now</NuxtLink
+				>
+				<NuxtLink to="/writings" class="opacity-50 hover:opacity-100"
+					>Writings</NuxtLink
+				>
+			</div>
+
+			<div class="flex items-center justify-end">
+				<UButton
+					color="neutral"
+					variant="soft"
+					class="cursor-pointer w-[28px] h-[28px] p-0 m-0 items-center justify-center"
+				>
+					<IconsMenu class="w-[16px] h-[16px]" />
+				</UButton>
+			</div>
+		</div>
+	</header>
+</template>
