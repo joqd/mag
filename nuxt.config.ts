65e0217b2cd3f5512997a666651efed439870611
joqd <gorgdis@gmail.com>
2025-08-31 22:19:41 +0330

    using nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-	modules: ["@nuxt/ui"],
+	modules: ["@nuxt/ui", "@nuxt/icon"],
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 	css: ["~/assets/css/main.css"],
