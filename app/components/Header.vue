6c8063b81816b05425986ad99247b5e7158e8c63
joqd <gorgdis@gmail.com>
2025-09-03 02:32:35 +0330

    pref: even better animation for menu slider :)

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import { motion, AnimatePresence } from "motion-v";
 import type { DropdownMenuItem } from "@nuxt/ui";
 import { useMediaQuery } from "@vueuse/core";
-import { motion, AnimatePresence } from "motion-v";
 
+// manage open/close menu
 const isMenuOpen = ref(false);
 
 function setBodyLock(lock: boolean) {
@@ -29,19 +30,20 @@ function closeMenu() {
 
 onBeforeUnmount(() => setBodyLock(false));
 
+// manage menu items
 const navbarItems = ref([
 	{ lable: "Home", to: "/" },
 	{ lable: "Now", to: "/now" },
 	{ lable: "Blog", to: "/blog" },
 ]);
 
-const primary: DropdownMenuItem[] = [
+const primaryMenuItems: DropdownMenuItem[] = [
 	{ label: "Home", icon: "lucide:home", to: "/" },
 	{ label: "Now", icon: "lucide:circle", to: "/now" },
 	{ label: "Blog", icon: "lucide:notebook", to: "/blog" },
 ];
 
-const secondary: DropdownMenuItem[] = [
+const secondaryMenuItems: DropdownMenuItem[] = [
 	{ label: "About", icon: "lucide:info", to: "/about" },
 	{ label: "Contact", icon: "lucide:contact-round", to: "/contact" },
 	{ label: "Resume", icon: "lucide:book-text", to: "/resume" },
@@ -50,13 +52,15 @@ const secondary: DropdownMenuItem[] = [
 const isSmUp = useMediaQuery("(min-width: 640px)");
 
 const menuItems = computed<DropdownMenuItem[]>(() =>
-	isSmUp.value ? secondary : [...primary, ...secondary],
+	isSmUp.value
+		? secondaryMenuItems
+		: [...primaryMenuItems, ...secondaryMenuItems],
 );
 </script>
 
 <template>
 	<div class="h-[30px]"></div>
-	<header class="relative w-full h-[60px] px-[20px] mb-[40px] z-50">
+	<header class="w-full h-[60px] px-[20px] mb-[40px] z-50">
 		<div
 			class="max-w-[440px] mx-auto grid grid-cols-2 sm:grid-cols-[max-content_minmax(0,1fr)_max-content] items-center"
 		>
@@ -118,16 +122,21 @@ const menuItems = computed<DropdownMenuItem[]>(() =>
 		<!-- Overlay -->
 		<AnimatePresence>
 			<motion.div
-				v-show="isMenuOpen"
+				v-if="isMenuOpen"
 				key="overlay"
-				class="fixed inset-0 bg-black/60"
+				class="fixed inset-0 z-40 bg-neutral-600/40 backdrop-blur-md pointer-events-auto"
 				:initial="{ opacity: 0 }"
 				:animate="{
 					opacity: 1,
-					transition: { duration: 0.3 },
+					transition: { duration: 0.75},
 				}"
-				:exit="{ opacity: 0, transition: { duration: 0.3 } }"
-				@click="toggleMenu"
+				:exit="{
+					opacity: 0,
+					transition: { duration: 0.75},
+				}"
+				role="button"
+				aria-label="Close menu overlay"
+				@click.self="toggleMenu"
 			/>
 		</AnimatePresence>
 
@@ -136,15 +145,15 @@ const menuItems = computed<DropdownMenuItem[]>(() =>
 			<motion.div
 				v-show="isMenuOpen"
 				key="panel"
-				class="fixed top-0 inset-x-0 min-h-80 bg-default pointer-events-auto w-full pt-[130px]"
+				class="fixed z-[41] top-0 inset-x-0 min-h-80 bg-default pointer-events-auto w-full pt-[130px]"
 				:initial="{ y: '-100%' }"
 				:animate="{
 					y: 0,
-					transition: { duration: 0.3, ease: [0.72, 0, 0.34, 1] },
+					transition: { duration: 0.75, ease: [0.22, 1, 0.36, 1] },
 				}"
 				:exit="{
 					y: '-100%',
-					transition: { duration: 0.3, ease: [0.72, 0, 0.34, 1] },
+					transition: { duration: 1, ease: [0.22, 1, 0.36, 1] },
 				}"
 			>
 				<div class="w-[480px] px-[20px] mx-auto space-y-5">
