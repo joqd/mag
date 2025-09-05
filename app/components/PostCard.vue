2079626e4f213ca5ecfe63bcdf389f655a49c3ef
joqd <gorgdis@gmail.com>
2025-09-05 11:15:44 +0330

    pref: seperate post cards

---
+++ app/components/PostCard.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+interface Props {
+	featured?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	featured: false,
+});
+</script>
+
+<template>
+	<div>
+		<div class="text-md font-bold line-clamp-1 cursor-default">
+			Effective Python Testing: Strategies and Tools
+		</div>
+		<div class="text-xs cursor-default opacity-90">
+			<UButton
+				color="neutral"
+				variant="link"
+				class="p-0 cursor-pointer font-normal underline"
+				>Tools</UButton
+			>
+			- December 2, 2023
+		</div>
+	</div>
+
+	<p
+		class="line-clamp-3 text-sm opacity-90 min-h-0 font-medium cursor-default"
+	>
+		Cover the importance of testing in Python development, exploring tools
+		and frameworks for unit testing, integration testing, and TDD
+	</p>
+
+	<div class="flex space-x-1">
+		<div v-if="props.featured">
+			<UTooltip
+				arrow
+				disableClosingTrigger
+				:ui="tooltipUi"
+				:delay-duration="0"
+				:content="{
+					align: 'start',
+					side: 'top',
+					sideOffset: 8,
+				}"
+				text="Featured Post"
+			>
+				<UButton
+					color="neutral"
+					variant="soft"
+					class="cursor-default w-[27.99px] h-[27.99px] p-0 m-0 items-center justify-center"
+				>
+					<IconsFeatured class="w-[12px] h-[12px]" />
+				</UButton>
+			</UTooltip>
+		</div>
+		<div>
+			<UButton
+				color="neutral"
+				variant="soft"
+				size="sm"
+				class="cursor-pointer"
+			>
+				Read More
+			</UButton>
+		</div>
+	</div>
+</template>
---
+++ app/components/PostList.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const allPostsLoaded = ref(false);
+</script>
+
+<template>
+	<div class="flex justify-between">
+		<h1 class="text-sm font-medium opacity-70 flex items-center px-[6px]">
+			Latest
+		</h1>
+		<div>
+			<UButton
+				variant="ghost"
+				color="neutral"
+				class="w-[30px] h-[30px] cursor-pointer p-0 m-0 items-center justify-center"
+			>
+				<IconsSearch class="w-[14px] h-[14px]" />
+			</UButton>
+		</div>
+	</div>
+	<div
+		class="w-full mt-[12px] px-[20px] py-[16px] border-2 border-default rounded-xl space-y-5"
+	>
+		<!-- posts -->
+		<div class="space-y-3">
+			<PostCard />
+			<USeparator size="sm" />
+			<PostCard featured />
+			<USeparator size="sm" />
+			<PostCard />
+			<USeparator size="sm" />
+		</div>
+
+		<!-- load more -->
+		<UButton
+			:disabled="allPostsLoaded"
+			variant="solid"
+			color="neutral"
+			class="w-full cursor-pointer"
+		>
+			<span class="mx-auto">Load More</span>
+		</UButton>
+	</div>
+</template>
